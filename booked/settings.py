c95ad640da7b3c06f9261320bca57de0df06ec4b
Yao Wang <ywang011@odu.edu>
2022-12-30 10:36:19 -0500

    update settings

--- booked/settings.py
+++ booked/settings.py
@@ -69,11 +69,12 @@ WSGI_APPLICATION = 'booked.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
-
+DATABASES['default'] = dj_database_url.config(default='postgres://iaqijptkisrsuw:8175e3f380b4b88d4973711a31648bd4d9dee22dc5fa882f81176b97e01c889e@ec2-34-207-12-160.compute-1.amazonaws.com:5432/d9pullo731s17e')
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
