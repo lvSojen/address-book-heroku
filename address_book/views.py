6ca0b47238f31df1e1c1f7e38626a292d3f70a36
Yao Wang <ywang011@odu.edu>
2022-12-30 15:53:02 -0500

    commit changes

--- address_book/views.py
+++ address_book/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from address_book.forms import AddressForm
+from address_book.models import Address
 
 def home(request) :
-    return render(request, 'pages/home.html', {})
+    all_addresses = Address.objects.all
+    context = {
+        'all_addresses' : all_addresses,
+    }
+    return render(request, 'pages/home.html', context)
 
 def add_address(request) :
-    return render(request, 'address/add_address.html', {})
+    all_fields = Address._meta.fields
+    context = {
+        'all_fields' : all_fields,
+    }
+    if request.method == 'POST':
+        form = AddressForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Address Has Been Added!'))
+            return redirect('home')
+        else:
+            messages.error(request, ('Seems like There was an Error...'))
+            return render(request, 'address/add_address.html', context)
+    else:
+        return render(request, 'address/add_address.html', context)
+
+def edit(request, list_id):
+	if request.method == 'POST':
+		current_address = Address.objects.get(pk=list_id)
+		form = AddressForm(request.POST or None, instance=current_address)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Address Has Been Edited!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Seems Like There Was An Error...'))
+			return render(request, 'address/edit.html', {})	
+	else:
+		get_address = Address.objects.get(pk=list_id)
+		return render(request, 'address/edit.html', {'get_address': get_address})
+
+def delete(request, list_id):
+	if request.method == 'POST':
+		current_address = Address.objects.get(pk=list_id)
+		current_address.delete()
+		messages.success(request, ('Address Has Been Deleted...'))
+		return redirect('home')
+	else:
+		messages.success(request, ('Nothing To See Here...'))
+		return redirect('home')
--- booked/settings.py
+++ booked/settings.py
@@ -3,7 +3,6 @@ import django_heroku
 from decouple import config
 import dj_database_url
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
